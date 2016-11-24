8a88c5115b8724bc03aeff553474f24584abd2e5
Richard <richardhayes777@gmail.com>
2016-11-24 16:34:54 +0000

    convenience for  getting a class double from a module

--- python/unittests.py
+++ python/unittests.py
@@ -4,6 +4,11 @@ import inspect
 import os
 
 
+def get_class_double(module, class_name):
+    classes = inspect.getmembers(module, inspect.isclass)
+    return filter(lambda cls: cls[0] == class_name, classes)[0]
+
+
 class BasicTestCase(unittest.TestCase):
     def setUp(self):
         self.maker = Maker()
@@ -22,19 +27,18 @@ class BasicTestCase(unittest.TestCase):
     def test_attribute_creation(self):
         cls = Class("Test")
         cls.add_attribute("first_name", "string")
+        # cls.add_attribute("flt", "double")
+        # cls.add_attribute("intg", "integer")
         self.maker.add_class(cls)
         self.maker.write()
         import model
 
         self.assertTrue("Test" in map(lambda parts: parts[0], inspect.getmembers(model, inspect.isclass)))
-        classes = inspect.getmembers(model, inspect.isclass)
+        
         att = filter(lambda att: att[0] == "first_name",
-                     inspect.getmembers(filter(lambda cls: cls[0] == "Test", classes)[0][1],
+                     inspect.getmembers(get_class_double(model, "Test")[1],
                                         lambda a: not (inspect.isroutine(a))))[0]
         self.assertEqual(att[0], "first_name")
-        # self.assertTrue("first_name" in
-        #                 map(lambda parts: parts[0], inspect.getmembers(inspect.getmembers(model, inspect.isclass)[1],
-        #                                                                lambda a: not (inspect.isroutine(a)))))
 
 
 if __name__ == "__main__":
