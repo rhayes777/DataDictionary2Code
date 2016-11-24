5bc6526ab362abfb8e654118fb7659cde46f69ea
Richard <richardhayes777@gmail.com>
2016-11-24 14:45:08 +0000

    added setUp and tearDown

--- python/unittests.py
+++ python/unittests.py
@@ -1,13 +1,19 @@
 import unittest
 import datadict2code
 import inspect
+import os
 
 
 class BasicTestCase(unittest.TestCase):
+    def setUp(self):
+        self.maker = datadict2code.Maker()
+
+    def tearDown(self):
+        os.system("rm model.py")
+
     def test_code_creation(self):
-        maker = datadict2code.Maker()
-        maker.add_class(class_name="Test")
-        maker.close()
+        self.maker.add_class(class_name="Test")
+        self.maker.close()
         import model
         self.assertTrue("Test" == inspect.getmembers(model, inspect.isclass)[0][0])
 
