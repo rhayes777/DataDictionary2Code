1d3ed8c24451ef00618ac261db828ad6205a4b6b
Richard <richardhayes777@gmail.com>
2016-11-24 14:38:05 +0000

    made newline indent a constant

--- python/datadict2code/__init__.py
+++ python/datadict2code/__init__.py
@@ -3,5 +3,5 @@ NEW_LINE_INDENT = "\n%s" % INDENT
 
 def create_class(name):
     text_file = open("classes.py", "w")
-    text_file.write("class %s:\n%spass" % (name, INDENT))
+    text_file.write("class %s:%spass" % (name, NEW_LINE_INDENT))
     text_file.close()
