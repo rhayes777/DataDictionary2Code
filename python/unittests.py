6fa9f01f77177448fd41dc91b82ef35af9c1889f
Richard <richardhayes777@gmail.com>
2016-11-25 09:29:53 +0000

    improved test unit functions

--- python/unittests.py
+++ python/unittests.py
@@ -3,18 +3,19 @@ from datadict2code import Maker, Class
 import inspect
 import os
 from sqlalchemy import DateTime, String, Integer, Float
+import model
 
 
-def get_class_double(module, class_name):
-    classes = inspect.getmembers(module, inspect.isclass)
+def get_class_double(class_name):
+    classes = inspect.getmembers(model, inspect.isclass)
     ls = filter(lambda cls: cls[0] == class_name, classes)
     if ls:
         return ls[0]
 
 
-def get_attribute_double(module, class_name, function_name):
+def get_attribute_double(class_name, function_name):
     ls = filter(lambda att: att[0] == function_name,
-                inspect.getmembers(get_class_double(module, class_name)[1],
+                inspect.getmembers(get_class_double(class_name)[1],
                                    lambda a: not (inspect.isroutine(a))))
     if ls:
         return ls[0]
@@ -31,7 +32,7 @@ class BasicTestCase(unittest.TestCase):
         self.maker.add_class(Class("Test"))
         self.maker.add_class(Class("SecondClass"))
         self.maker.write()
-        import model
+        reload(model)
         self.assertTrue("Test" in map(lambda parts: parts[0], inspect.getmembers(model, inspect.isclass)))
         self.assertTrue("SecondClass", inspect.getmembers(model, inspect.isclass)[1][0])
 
@@ -43,19 +44,19 @@ class BasicTestCase(unittest.TestCase):
         cls.add_attribute("dt", "datetime")
         self.maker.add_class(cls)
         self.maker.write()
-        import model
+        reload(model)
 
         self.assertTrue("Test" in map(lambda parts: parts[0], inspect.getmembers(model, inspect.isclass)))
 
-        self.assertEqual(get_attribute_double(model, "Test", "first_name")[0], "first_name")
-        self.assertEqual(get_attribute_double(model, "Test", "flt")[0], "flt")
-        self.assertEqual(get_attribute_double(model, "Test", "intg")[0], "intg")
-        self.assertEqual(get_attribute_double(model, "Test", "dt")[0], "dt")
+        self.assertEqual(get_attribute_double("Test", "first_name")[0], "first_name")
+        self.assertEqual(get_attribute_double("Test", "flt")[0], "flt")
+        self.assertEqual(get_attribute_double("Test", "intg")[0], "intg")
+        self.assertEqual(get_attribute_double("Test", "dt")[0], "dt")
 
-        self.assertTrue(isinstance(get_attribute_double(model, "Test", "dt")[1].type, DateTime))
-        self.assertTrue(isinstance(get_attribute_double(model, "Test", "flt")[1].type, Float))
-        self.assertTrue(isinstance(get_attribute_double(model, "Test", "intg")[1].type, Integer))
-        self.assertTrue(isinstance(get_attribute_double(model, "Test", "first_name")[1].type, String))
+        self.assertTrue(isinstance(get_attribute_double("Test", "dt")[1].type, DateTime))
+        self.assertTrue(isinstance(get_attribute_double("Test", "flt")[1].type, Float))
+        self.assertTrue(isinstance(get_attribute_double("Test", "intg")[1].type, Integer))
+        self.assertTrue(isinstance(get_attribute_double("Test", "first_name")[1].type, String))
 
     def test_relationships(self):
         parent = Class("Parent")
@@ -65,9 +66,10 @@ class BasicTestCase(unittest.TestCase):
         self.maker.add_class(parent)
         self.maker.add_class(child)
         self.maker.write()
-        import model
+        reload(model)
 
-        self.assertEqual(get_attribute_double(model, "Parent", "children")[0], "children")
+        print(inspect.getmembers(model, inspect.isclass))
+        self.assertEqual(get_attribute_double("Parent", "children")[0], "children")
 
 
 if __name__ == "__main__":
